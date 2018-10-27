3b707068bbcf0d16eadc59a3a1e25780704f1e23
Brody Williams <brody.williams@ttu.edu>
2018-10-27 13:32:53 -0500

    hardware tests

--- hardware/LRR_Hardware_Controller.py
+++ hardware/LRR_Hardware_Controller.py
@@ -15,6 +15,8 @@ Update database
 
 Magnetic Contact Switch
     - Use GPIO Zero library?
+    - Could also use button on breadboard/Sense hat joystick to simulate
+    as suggested
     
 Use LED to display running/not running status? -Green/Red?
 '''
