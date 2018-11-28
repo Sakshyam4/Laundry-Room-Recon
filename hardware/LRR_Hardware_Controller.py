0a9cb87b4ad33f475e7c085420097157447e6d4c
Brody Williams <brody.williams@ttu.edu>
2018-11-28 11:41:26 -0600

    database adjustments/door swtich in-progress

--- hardware/LRR_Hardware_Controller.py
+++ hardware/LRR_Hardware_Controller.py
@@ -2,41 +2,30 @@
 # Brody Williams
 
 '''
-Necessary Functionality:
-
-Retrieve Sensor Data - IMU Unit & Magnetic Contact Switch
-    - Appropriate IMU sensor sensitivity - Automatic
-    - How often? .5 seconds?
-    - Accelerometer raw data for vibration? - Working
-
-Make device status determinations based on sensor data
-
-Update database
-    - How often? - only when status changes
-
 Magnetic Contact Switch
     - Use GPIO Zero library?
     - Could also use button on breadboard/Sense hat joystick to simulate
     as suggested by Dr. Lim
-    
-Use LED to display running/not running status? -Green/Red? - Working
 '''
+
 from sense_hat import SenseHat
+from firebase import firebase
 import time
 import copy
 
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://laundry-room-recon.firebaseio.com',None)
+# Debug Constant (Very C-esque, I know)
+DEBUG = 1
 
-#Globals
-R = (255,0,0) #Red
-G = (0,255,0) #Green
-
-sense = SenseHat()
+# Globals
+R = (255,0,0) # Red
+G = (0,255,0) # Green
 
 vib_threshold = 2
 
-running_image = [
+sense = SenseHat()
+firebase = firebase.FirebaseApplication('https://laundry-room-recon.firebaseio.com', None)
+
+running_dclosed = [
 G, G, G, G, G, G, G, G,
 G, G, G, G, G, G, G, G,
 G, G, G, G, G, G, G, G,
@@ -47,7 +36,18 @@ G, G, G, G, G, G, G, G,
 G, G, G, G, G, G, G, G
 ]
 
-stopped_image = [
+running_dopen = [
+G, G, G, R, R, G, G, G,
+G, G, G, R, R, G, G, G,
+G, G, G, R, R, G, G, G,
+G, G, G, R, R, G, G, G,
+G, G, G, R, R, G, G, G,
+G, G, G, G, G, G, G, G,
+G, G, G, R, R, G, G, G,
+G, G, G, R, R, G, G, G
+]
+
+stopped_dclosed = [
 R, R, R, R, R, R, R, R,
 R, R, R, R, R, R, R, R,
 R, R, R, R, R, R, R, R,
@@ -58,89 +58,115 @@ R, R, R, R, R, R, R, R,
 R, R, R, R, R, R, R, R
 ]
 
+stopped_dopen = [
+R, R, R, G, G, R, R, R,
+R, R, R, G, G, R, R, R,
+R, R, R, G, G, R, R, R,
+R, R, R, G, G, R, R, R,
+R, R, R, G, G, R, R, R,
+R, R, R, R, R, R, R, R,
+R, R, R, G, G, R, R, R,
+R, R, R, G, G, R, R, R
+]
+
+
 # Main function for LRR hardware that loops
 # and calls other functions as necessary.
 def run_lrr():
-    #Initialize values/display
+    # Initialize values/display
     prev_raw_data = {'x': 0, 'y': 1.9, 'z': 95}
-    prev_status = 0;
-    sense.set_pixels(stopped_image)
+    prev_running_status = 0 # 0: Not running 1: Running
+    prev_door_status = 0 # 0: Door closed 1: Door open
+    sense.set_pixels(stopped_dclosed)
     
     while True:
-        #Gather raw accelerometer data
+        # Gather raw accelerometer data
         raw_data = sense.get_accelerometer_raw()
 
-        #Modify raw data for easier visualization
+        # Modify raw data for easier visualization
         raw_data['x'] *= 100
         raw_data['y'] *= 100
         raw_data['z'] *= 100
         
-        #Get new device status
-        status = determine_status(prev_raw_data, raw_data)
-        
-        #Debugging/Sensitivity adjustment output
-        debug_print(prev_raw_data, raw_data, prev_status, status)
+        # Get new running status
+        running_status = determine_running_status(prev_raw_data, raw_data)
 
-        #Update LED display and database if status changes
-        if(status != prev_status):
-            display_status(status)
-            update_db(status)
+        # Get new door status - TODO
+        door_status = determine_door_status()
+
+        # Debugging/Sensitivity adjustment output
+        if DEBUG:
+            debug_print(prev_raw_data, raw_data, prev_running_status, \
+                        running_status, prev_door_status, door_status)
+
+        # Update LED display and database if either status changes
+        if (running_status != prev_running_status) or (door_status != prev_door_status):
+            display_status(running_status, door_status)
+            update_db(running_status, door_status)
             
-        #Update previous values and wait before next loop iteration
+        # Update previous values and wait before next loop iteration
         prev_raw_data = copy.deepcopy(raw_data)
-        prev_status = status
+        prev_running_status = running_status
+        prev_door_status = door_status
         time.sleep(.5)
 
 # Determines running/not running status by comparing
 # raw accelerometer data with previous data. Returns
 # 1 for running or 0 for not running.
-def determine_status(prev_data, new_data):
+def determine_running_status(prev_data, new_data):
     if(abs(new_data['x'] - prev_data['x']) > vib_threshold) or \
         (abs(new_data['y'] - prev_data['y']) > vib_threshold) or \
         (abs(new_data['z'] - prev_data['z']) > vib_threshold):
         return 1
     else:
         return 0
-
-#Updates the Sense Hat LED display to reflect the current status
-def display_status(status):
-    if(status == 1):
-        sense.set_pixels(running_image)
-    else:
-        sense.set_pixels(stopped_image)
-
-#Updates the firebase database with the new status
-def update_db(status):
-    #Do database stuff
     
-    if status == 0:
-        #curl -X PUT -d '{"washingMachine": "0"}'\
-        #     'https://laundry-room-recon.firebaseio.com'
-        firebase.put('https://laundry-room-recon.firebaseio.com','washingMachine/runningStatus',0)
-        status_string = '0'
-    elif status == 1:
-        #curl -X PUT -d '{"washingMachine": "1"}'\
-         #    'https://laundry-room-recon.firebaseio.com'
-        firebase.put('https://laundry-room-recon.firebaseio.com','washingMachine/runningStatus',1)
-        status_string = '1'
-
-    else:
-        status_string = "Problem"
-    print("\nUpdating database with {:s} status!\n" .format(status_string))
+#TODO
+def determine_door_status():
+    pass
     
-    #wmstatus.setValue(status_string)
+# Updates the Sense Hat LED display to reflect the current status
+def display_status(running_status, door_status):
+    # Device stopped / Door closed
+    if(running_status == 0) and (door_status == 0):
+        sense.set_pixels(stopped_dclosed)
+    # Device stopped / Door open
+    elif (running_status == 0) and (door_status == 1):
+        sense.set_pixels(stopped_dopen)
+    # Device running / Door closed
+    elif (running_status == 1) and (door_status == 0):
+        sense.set_pixels(running_dclosed)
+    # Device running / Door open (possible for top load washers?)
+    elif (running_status == 1) and (door_status == 1):
+        sense.set_pixels(running_dopen)
 
-#Function to print x,y,z values while developing/debugging
-def debug_print(prev_data, new_data, prev_status, status):
+# Updates the firebase database with the new status
+def update_db(running_status, door_status):
+
+    # Update the database
+    firebase.put('https://laundry-room-recon.firebaseio.com','washingMachine/runningStatus', running_status)
+    firebase.put('https://laundry-room-recon.firebaseio.com','washingMachine/doorStatus', door_status)
+
+    if DEBUG:
+        # Print new status
+        print("\nUpdating database! Running status: {:d} Door status: {:d}\n" .format(running_status, door_status))
+    
+
+# Function to print x,y,z  vibration data values & current/previous
+# statuses while developing/debugging
+def debug_print(prev_raw_data, raw_data, prev_running_status, \
+                    running_status, prev_door_status, door_status):
     print("\n Previous Data: x = %.5f, y = %.5f, z = %.5f," \
           "\n New Data :     x = %.5f, y = %.5f, z = %.5f," \
           "\n D(x) = %.5f, D(y) = %.5f, D(z) = %.5f" \
-          "\n Previous Status: %d New Status: %d\n" \
+          "\n Previous Running Status: %d New Running Status: %d\n" \
+          "\n Previous Door Status %d New Door Status: %d\n" \
            % (prev_data['x'], prev_data['y'], prev_data['z'], \
                   new_data['x'], new_data['y'], new_data['z'], \
                   abs(new_data['x'] - prev_data['x']), \
                   abs(new_data['y'] - prev_data['y']), \
                   abs(new_data['z'] - prev_data['z']), \
-                  prev_status, status))
+                  prev_running_status, running_status, \
+                  prev_door_status, door_status))
     
     
